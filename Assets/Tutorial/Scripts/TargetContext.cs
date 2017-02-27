4af97a4123a7f02ed5c596707bd69e7839855a38
busskiller <busskiller@gmail.com>
2017-02-27 19:40:04 +0100

    v0.01
    rr

---
+++ Assets/Tutorial/Scripts/TargetContext.cs
@@ -0,0 +1,24 @@
+﻿namespace MyNameSpace1
+{
+    using Apex.AI;
+    using UnityEngine;
+
+    public sealed class TargetContext : IAIContext
+    {
+
+
+        public TargetContext(Transform transform, GameObject[] targets)
+        {
+            this.self = transform;
+            this.targets = targets;
+
+        }
+
+        public Transform self { get; private set; }
+
+        public GameObject[] targets { get; private set; }
+
+    }
+
+}
+
---
+++ Assets/Tutorial/Scripts/TargetContextProvider.cs
@@ -0,0 +1,33 @@
+﻿using Apex.AI.Components;
+
+namespace MyNameSpace1
+{
+
+    using System;
+    using UnityEngine;
+    using Apex.AI;
+
+
+    public class TargetContextProvider : MonoBehaviour, IContextProvider
+    {
+
+
+        [SerializeField]
+        private GameObject[] _targets = new GameObject[0];
+
+        private TargetContext _context;
+
+        public void OnEnable()
+        {
+            _context = new TargetContext(this.transform, _targets);
+        }
+
+        public IAIContext GetContext(Guid aiId)
+        {
+            return _context;
+        }
+
+    }
+
+}
+
